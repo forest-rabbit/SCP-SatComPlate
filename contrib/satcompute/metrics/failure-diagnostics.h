ebd3c638e5ee008007fe9d1aa9842608d79c71a1
Emsky <2796353286@qq.com>
2026-07-27 12:10:53 +0800

    refactor: isolate failure diagnostics

---
+++ contrib/satcompute/metrics/failure-diagnostics.h
@@ -0,0 +1,45 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef SATCOMPUTE_FAILURE_DIAGNOSTICS_H
+#define SATCOMPUTE_FAILURE_DIAGNOSTICS_H
+
+#include "flow-metrics.h"
+
+#include <string>
+#include <vector>
+
+namespace ns3 {
+
+class TaskCoordinator;
+
+void RemoveFailureDiagnosticOutputs(const std::string& outputDirectory);
+
+void WriteFailureDiagnostics(
+  const FlowAggregate& aggregate,
+  double simulationDurationSeconds,
+  const RunMetadata& runMetadata,
+  const std::vector<TransferFlowMetadata>& transferFlows,
+  const std::vector<TransferSummaryRecord>& transferSummaries,
+  const std::vector<EcmpRouteDecisionEvent>& routeEvents,
+  const std::vector<IslDirectedLink>& directedLinks,
+  const std::vector<IslQueueDropEvent>& queueDropEvents,
+  const TaskCoordinator& coordinator,
+  const std::string& outputDirectory);
+
+} // namespace ns3
+
+#endif
